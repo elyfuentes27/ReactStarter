f78a4dbc7a6b71cd546658e2710e5f5bc10f905c
github username <github email address>
2019-07-20 19:55:22 -0400

    changed the color of the text from black to white in result page

--- src/pages/result.js
+++ src/pages/result.js
@@ -19,7 +19,7 @@ const useStyles = makeStyles(theme => ({
     root: {
         width: '100%',
         maxWidth: 360,
-        color: 'black',
+        color: 'white',
         backgroundColor: 'transparent'
     },
 
