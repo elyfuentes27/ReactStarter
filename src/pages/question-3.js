848f9dab6bd9f9746ea48cfab88f8c837233b0ec
Ely Fuentes <elyfuentes@ip-192-168-0-158.ec2.internal>
2019-07-20 19:49:19 -0400

    Finished fixing checkbox

--- src/pages/question-3.js
+++ src/pages/question-3.js
@@ -7,58 +7,75 @@ import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 
-const ThirdPage = () => (
-  <Layout>
-    <div className="body-margin-top" />
-    {/* Question 1 */}
-    <p>Does your app needs animations and transitions?</p>
-    <RadioGroup
-          aria-label="Form"
-          name="form"
-          className=""
-          value=""
-          onChange=""
-        >
-          <FormControlLabel value="female" control={<Radio />} label="Animations" />
-          <FormControlLabel value="male" control={<Radio />} label="Transitions" />
-          <FormControlLabel value="male" control={<Radio />} label="No" />
-    </RadioGroup> 
-    {/* Question 2 */}
-    <p>Do users have different permissions across the app?</p>
-    <RadioGroup
-          aria-label="Form"
-          name="form"
-          className=""
-          value=""
-          onChange=""
-        >
-          <FormControlLabel value="female" control={<Radio />} label="Multiple types of users" />
-          <FormControlLabel value="male" control={<Radio />} label="No" />
-    </RadioGroup> 
-    {/* Question 3 */}
-    <p>Does your app will be updated in real time?</p>
-    <RadioGroup
-          aria-label="Form"
-          name="form"
-          className=""
-          value=""
-          onChange=""
-        >
-          <FormControlLabel value="female" control={<Radio />} label="Request based" />
-          <FormControlLabel value="male" control={<Radio />} label="Real Time" />
-    </RadioGroup> 
+const ThirdPage = () => {
+  const [value, setValue] = React.useState('');
+  const [value2, setValue2] = React.useState('');
+  const [value3, setValue3] = React.useState('');
 
-    <div className="full-width-ctas">
-      <Link to="/">
-        <Cta text="Back" arrowType="back"/>
-      </Link>
+  function handleChange(event) {
+      setValue(event.target.value);
+  }
 
-      <Link to="/question-4/">
-        <Cta text="Next" arrowType="next"/>
-      </Link>
-    </div>
-    
-  </Layout>
-)
+  function handleChange2(event) {
+      setValue2(event.target.value);
+  }
+
+  function handleChange3(event) {
+      setValue3(event.target.value);
+  }
+  return (
+    <Layout>
+        <div className="body-margin-top" />
+        {/* Question 1 */}
+        <p>Does your app needs animations and transitions?</p>
+        <RadioGroup
+            aria-label="Form"
+            name="form"
+            className=""
+            value={value}
+            onChange={handleChange}
+            >
+            <FormControlLabel value="animation" control={<Radio />} label="Animations" />
+            <FormControlLabel value="transitions" control={<Radio />} label="Transitions" />
+            <FormControlLabel value="no" control={<Radio />} label="No" />
+        </RadioGroup> 
+        {/* Question 2 */}
+        <p>Does users have different permissions across the app?</p>
+        <RadioGroup
+            aria-label="Form"
+            name="form"
+            className=""
+            value={value2}
+            onChange={handleChange2}
+            >
+            <FormControlLabel value="multipleTypes" control={<Radio />} label="Multiple types of users" />
+            <FormControlLabel value="noPermission" control={<Radio />} label="No" />
+        </RadioGroup> 
+        {/* Question 3 */}
+        <p>Does your app will be updated in real time?</p>
+        <RadioGroup
+            aria-label="Form"
+            name="form"
+            className=""
+            value={value3}
+            onChange={handleChange3}
+            >
+            <FormControlLabel value="requestBased" control={<Radio />} label="Request based" />
+            <FormControlLabel value="RealTime" control={<Radio />} label="Real Time" />
+        </RadioGroup> 
+
+        <div className="full-width-ctas">
+        <Link to="/">
+            <Cta text="Back" arrowType="back"/>
+        </Link>
+
+        <Link to="/question-4/">
+            <Cta text="Next" arrowType="next"/>
+        </Link>
+        </div>
+        
+    </Layout>
+    )
+}
 
 export default ThirdPage
--- src/pages/question-4.js
+++ src/pages/question-4.js
@@ -8,54 +8,71 @@ import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 
-const FourthPage = () => (
-  <Layout>
-    <div className="body-margin-top" />
-    {/* Question 1 */}
-    <p>Do have specific design needs (custom design)?</p>
-    <RadioGroup
-          aria-label="Form"
-          name="form"
-          className=""
-          value=""
-          onChange=""
-        >
-          <FormControlLabel value="female" control={<Radio />} label="Low level of customization" />
-          <FormControlLabel value="male" control={<Radio />} label="High level of customization" />
-    </RadioGroup> 
-    {/* Question 2 */}
-    <p>Approximate number of components including nested components</p>
-    <RadioGroup
-          aria-label="Form"
-          name="form"
-          className=""
-          value=""
-          onChange=""
-        >
-          <FormControlLabel value="female" control={<Radio />} label="30" />
-          <FormControlLabel value="male" control={<Radio />} label="10+" />
-          <FormControlLabel value="male" control={<Radio />} label="300+" />
-    </RadioGroup> 
-    {/* Question 3 */}
-    <p>Does your application requires themes?</p>
-    <RadioGroup
-          aria-label="Form"
-          name="form"
-          className=""
-          value=""
-          onChange=""
-        >
-          <FormControlLabel value="female" control={<Radio />} label="Single theme" />
-          <FormControlLabel value="male" control={<Radio />} label="Multiple themes" />
-          <FormControlLabel value="male" control={<Radio />} label="No" />
-    </RadioGroup> 
+const FourthPage = () => {
+  const [value, setValue] = React.useState('');
+  const [value2, setValue2] = React.useState('');
+  const [value3, setValue3] = React.useState('');
 
-    <div className="full-width-ctas">
-      <Link to="/result/">
-        <Cta text="Finish" arrowType="next"/>
-      </Link>
-    </div>
-  </Layout>
-)
+  function handleChange(event) {
+      setValue(event.target.value);
+  }
+
+  function handleChange2(event) {
+      setValue2(event.target.value);
+  }
+
+  function handleChange3(event) {
+      setValue3(event.target.value);
+  }
+  return (
+    <Layout>
+        <div className="body-margin-top" />
+        {/* Question 1 */}
+        <p>Do have specific design needs (custom design)?</p>
+        <RadioGroup
+            aria-label="Form"
+            name="form"
+            className=""
+            value={value}
+            onChange={handleChange}
+            >
+            <FormControlLabel value="lowLevel" control={<Radio />} label="Low level of customization" />
+            <FormControlLabel value="HighLevel" control={<Radio />} label="High level of customization" />
+        </RadioGroup> 
+        {/* Question 2 */}
+        <p>Approximate number of components including nested components</p>
+        <RadioGroup
+            aria-label="Form"
+            name="form"
+            className=""
+            value={value2}
+            onChange={handleChange2}
+            >
+            <FormControlLabel value="30selected" control={<Radio />} label="30" />
+            <FormControlLabel value="10moreSelected" control={<Radio />} label="10+" />
+            <FormControlLabel value="300orMoreSelected" control={<Radio />} label="300+" />
+        </RadioGroup> 
+        {/* Question 3 */}
+        <p>Does your application requires themes?</p>
+        <RadioGroup
+            aria-label="Form"
+            name="form"
+            className=""
+            value={value3}
+            onChange={handleChange3}
+            >
+            <FormControlLabel value="singleTheme" control={<Radio />} label="Single theme" />
+            <FormControlLabel value="multipleThemes" control={<Radio />} label="Multiple themes" />
+            <FormControlLabel value="noTheme" control={<Radio />} label="No" />
+        </RadioGroup> 
+
+        <div className="full-width-ctas">
+        <Link to="/result/">
+            <Cta text="Finish" arrowType="next"/>
+        </Link>
+        </div>
+    </Layout>
+    )
+  }
 
 export default FourthPage
