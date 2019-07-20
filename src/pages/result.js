e2f1f9a473c4922280067958db9d4264459569ec
Ely Fuentes <elyfuentes@ip-192-168-0-158.ec2.internal>
2019-07-20 17:48:23 -0400

    List Styled

--- src/pages/result.js
+++ src/pages/result.js
@@ -4,14 +4,49 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const result = () => (
-  <Layout>
-    <SEO title="result" />
-    <h1>Result</h1>
-    <Link to="/question-4/">Go back to Question 4</Link>
-    <br/>
-    <Link to="/">Go Home</Link>
-  </Layout>
-)
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
-export default result
+import toolsResult from '../common/toolsResults'
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: '100%',
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
+
+function ListItemLink(props) {
+    return <ListItem button component = "a" {
+        ...props
+    }
+    />;
+}
+export default function result() {
+  const classes = useStyles();
+    return (
+        <Layout>
+            <SEO title="result" />
+            <h1>Result</h1>
+
+            <List component="nav" className={classes.root} aria-label="Contacts">
+            {toolsResult.map( i => {
+                if(i.name === 'Forms'){
+                    return (
+                        <div>
+                        {i.links.map( list => <ListItemLink href={list.link}><ListItemText inset primary={list.name} /></ListItemLink>)}
+                        </div>
+                    )
+                }
+            })}
+            </List>
+
+            <Link to="/question-4/">Go back to Question 4</Link>
+            <br/>
+            <Link to="/">Go Home</Link>
+        </Layout>
+    )
+}
--- src/pages/toolResultsPage.js
+++ src/pages/toolResultsPage.js
@@ -3,25 +3,51 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import allCommands from '../common/toolsResults'
+import toolsResult from '../common/toolsResults'
 
-const result = () => (
-  <Layout>
-    <SEO title="tool result" />
-    <h1>Tools Link</h1>
-    <div>
-        {allCommands.map( (i, index) => 
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: '100%',
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
+
+function ListItemLink(props) {
+    return <ListItem button component = "a" {
+        ...props
+    }
+    />;
+}
+
+export default function toolResult() {
+  const classes = useStyles();
+    return (
+        <Layout>
+            <SEO title="tool result" />
+            <h1>Tools Link</h1>
             <div>
-            <h3>{i.name}</h3>
-                <div>
-                    {i.links.map( list => <a href={list.link} target="_blank"><li>{list.name}</li></a>)}
-                </div>
+                {toolsResult.map( (i, index) => 
+                    <div>
+                    <h3>{i.name}</h3>
+                        <div>
+                            <List component="nav" className={classes.root} aria-label="Contacts">
+                                <div>
+                                    {i.links.map( list => <ListItemLink href={list.link}><ListItemText inset primary={list.name} /></ListItemLink>)}
+                                </div>
+                            </List>
+                        </div>
+                    </div>
+                    )}
             </div>
-            )}
-    </div>
-    <br/>
-    <Link to="/">Go Home</Link>
-  </Layout>
-)
-
-export default result
+            <br/>
+            <Link to="/">Go Home</Link>
+        </Layout>
+        )
+}
